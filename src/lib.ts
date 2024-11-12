2f2f85f975abf4ef8e971c17d561ce8c06f03e8f
chuanqisun <chuanqi.sun.dev@gmail.com>
2024-11-12 10:53:03 -0800

    add public assets

---
+++ src/lib.ts
@@ -0,0 +1,5 @@
+import { definePespectiveCarousel } from "./perspective-carousel/perspective-carousel";
+
+export * from "./perspective-carousel/perspective-carousel";
+
+definePespectiveCarousel();
