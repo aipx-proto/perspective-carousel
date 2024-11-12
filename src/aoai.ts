e97c3353fad1d7f0ec30dc372894cfde3a92fcb4
chuanqisun <chuanqi.sun.dev@gmail.com>
2024-11-12 10:41:44 -0800

    fix auth

--- src/aoai.ts
+++ src/aoai.ts
@@ -4,6 +4,8 @@ import { AzureOpenAI } from "openai";
 const credential = new InteractiveBrowserCredential({
   tenantId: "72f988bf-86f1-41af-91ab-2d7cd011db47",
   clientId: "6e4c509e-2b65-40dc-b461-beb2c824f63a",
+  redirectUri: location.href,
+  loginStyle: "redirect",
 });
 const azureADTokenProvider = getBearerTokenProvider(credential, []);
 export const aoai = new AzureOpenAI({ azureADTokenProvider, apiVersion: "2024-07-01-preview", endpoint: "https://proto-api.azure-api.net/" });
