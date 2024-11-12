d37e181ae9a3ca9f037ed77c4b0c93e5d290dccf
chuanqisun <chuanqi.sun.dev@gmail.com>
2024-11-11 16:58:09 -0800

    reduce component surface

--- src/perspective-carousel/perspective-carousel.ts
+++ src/perspective-carousel/perspective-carousel.ts
@@ -1,4 +1,3 @@
-import { layouts } from "./layouts";
 import "./perspective-carousel.css";
 
 declare global {
@@ -37,7 +36,7 @@ export class PerspectiveCarouselElement extends HTMLElement {
   connectedCallback() {
     this.items = [...this.querySelectorAll<HTMLElement>("carousel-item")!];
 
-    const matchingLayout = layouts.find((layout) => layout.forLength === this.items.length);
+    const matchingLayout = getLayouts().find((layout) => layout.forLength === this.items.length);
     if (!matchingLayout) throw new Error(`No layout found for ${this.items.length} items. We only support 3, 4, or 5 items`);
     this.layout = matchingLayout.layout;
 
@@ -116,6 +115,102 @@ export class PerspectiveCarouselElement extends HTMLElement {
       item.dataset.distance = (this.layout.length - zIndex).toString();
     });
   }
+}
+
+function getLayouts() {
+  return [
+    {
+      forLength: 3,
+      layout: [
+        /** 3 inline */
+        {
+          translate: -30,
+          scale: 0.6,
+          zIndex: 2,
+          reverseZIndex: 1,
+        },
+        {
+          translate: 0,
+          scale: 1,
+          zIndex: 3,
+          reverseZIndex: 3,
+        },
+        {
+          translate: 30,
+          scale: 0.6,
+          zIndex: 1,
+          reverseZIndex: 2,
+        },
+      ],
+    },
+    {
+      forLength: 4,
+      layout: [
+        /** 3 front, 1 back */
+        {
+          translate: -30,
+          scale: 0.6,
+          zIndex: 4,
+          reverseZIndex: 4,
+        },
+        {
+          translate: 0,
+          scale: 1,
+          zIndex: 3,
+          reverseZIndex: 3,
+        },
+        {
+          translate: 30,
+          scale: 0.6,
+          zIndex: 2,
+          reverseZIndex: 2,
+        },
+        {
+          translate: 0,
+          scale: 0.35,
+          zIndex: 1,
+          reverseZIndex: 1,
+        },
+      ],
+    },
+    {
+      forLength: 5,
+      layout:
+        /** 3 front, 2 back */
+        [
+          {
+            translate: -30,
+            scale: 0.6,
+            zIndex: 4,
+            reverseZIndex: 3,
+          },
+          {
+            translate: 0,
+            scale: 1,
+            zIndex: 5,
+            reverseZIndex: 5,
+          },
+          {
+            translate: 30,
+            scale: 0.6,
+            zIndex: 3,
+            reverseZIndex: 4,
+          },
+          {
+            translate: 15,
+            scale: 0.35,
+            zIndex: 2,
+            reverseZIndex: 2,
+          },
+          {
+            translate: -15,
+            scale: 0.35,
+            zIndex: 1,
+            reverseZIndex: 1,
+          },
+        ],
+    },
+  ];
 }
 
 export function definePespectiveCarousel(tagName = "perspective-carousel") {
